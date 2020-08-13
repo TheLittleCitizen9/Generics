1a1e81db511f15454e3ce58d89c51e38ee331b3d
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 17:02:05 +0300

    ReplaceAWithSixBlock class

---
+++ Generics/Generics/ReplaceAWithSixBlock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class ReplaceAWithSixBlock : IBlock<string>
+    {
+        public string Process(string input)
+        {
+            if (input != string.Empty)
+            {
+                Char[] chars = input.ToCharArray();
+                if(chars.Contains('A'))
+                {
+                    for (int i = 0; i < chars.Length; i++)
+                    {
+                        if(chars[i] == 'A')
+                        {
+                            chars[i] = '6';
+                        }
+                    }
+                    return new string(chars);
+                }
+                else
+                {
+                    return input;
+                }
+                
+            }
+            else
+            {
+                throw new ArgumentException("The input is an empty string");
+            }
+        }
+    }
+}
