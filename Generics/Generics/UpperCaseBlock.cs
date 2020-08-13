dc84827c7ce65896c46e1cdc024dc2c353151aad
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 16:50:19 +0300

    Added UpperCaseBlock class

---
+++ Generics/Generics/UpperCaseBlock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class UpperCaseBlock : IBlock<string>
+    {
+        public string Process(string input)
+        {
+            if(input != string.Empty)
+            {
+                return input.ToUpper();
+            }
+            else
+            {
+                throw new ArgumentException("The input is an empty string");
+            }
+        }
+    }
+}
