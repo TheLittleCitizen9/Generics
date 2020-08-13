37636fdaf71175b22a4c87db573c85917d99012d
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 16:57:15 +0300

    Added ReverseBlock class

---
+++ Generics/Generics/ReverseBlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class ReverseBlock : IBlock<string>
+    {
+        public string Process(string input)
+        {
+            if(input != string.Empty)
+            {
+                Char[] chars = input.ToCharArray();
+                var reversedChars = chars.Reverse().ToArray();
+                return new string(reversedChars);
+            }
+            else
+            {
+                throw new ArgumentException("The input is an empty string");
+            }
+        }
+    }
+}
