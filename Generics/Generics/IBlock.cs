a0dc8af688d8f723812dbc4b753d907cd9b79fb8
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 16:28:22 +0300

    Added IBlock and IDataFlow

---
+++ Generics/Generics/IBlock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public interface IBlock<T> where T : class
+    {
+        T Process(T input);
+    }
+
+}
